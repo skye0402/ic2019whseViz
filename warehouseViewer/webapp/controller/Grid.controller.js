1a920bc61acdf32b630bc538399a481c2a4129b9
Haruyuki-Kobayashi <haruyuki.koba@gmail.com>
2019-10-31 04:42:04 +0000

    Add Back button to Schedule and Status tile

--- warehouseViewer/webapp/controller/Grid.controller.js
+++ warehouseViewer/webapp/controller/Grid.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
 		'sap/m/MessageToast',
 		'sap/ui/core/mvc/Controller',
-		'sap/ui/model/json/JSONModel'
+		'sap/ui/model/json/JSONModel',
+		"sap/ui/core/routing/History"
 	],
-	function (MessageToast, Controller, JSONModel) {
+	function (MessageToast, Controller, JSONModel, History) {
 		"use strict";
 
 		var PageController = Controller.extend("warehouseViewer.warehouseViewer.controller.Grid", {
@@ -14,6 +15,17 @@ sap.ui.define([
 			},
 			press: function (oEvent) {
 				MessageToast.show("The radial micro chart is pressed.");
+			},
+			onNavBack: function () {
+				var oHistory = History.getInstance();
+				var sPreviousHash = oHistory.getPreviousHash();
+
+				if (sPreviousHash !== undefined) {
+					window.history.go(-1);
+				} else {
+					var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+					oRouter.navTo("home", true);
+				}
 			}
 		});
 
